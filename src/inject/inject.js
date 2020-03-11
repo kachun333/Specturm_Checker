f9b1df4716de5c68366ff3db286d7b6a971dda8a
1998chun <34387770+1998chun@users.noreply.github.com>
2020-03-12 01:28:47 +0800

    get & store dom content

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,27 +6,86 @@ chrome.extension.sendMessage({}, function (response) {
 			// ----------------------------------------------------------
 			// This part of the script triggers when page is done loading
 			console.log("Hello. This message was sent from scripts/inject.js");
-			const domObj = $('#region-main');
-			console.log(domObj.children())
-			// const result = updateLocalStorage(domObj);
-			// console.log(result)
+
+			const courseTitle = $(".page-header-headings h1")[0].innerText || $("title")[0].innerText;
+			const prevDom = getPrevDom(courseTitle);
+			const currentDom = getCurrentDom();
+
+			if (!prevDom) {
+				storeDom(courseTitle, currentDom);
+			} else {
+				//compare Dom
+				let changes = null;
+
+				currentDom.topics.forEach(currentDomTopic => {
+					let isOldTopic = false;
+					prevDom.topics.forEach(prevDomTopic => {
+						if (currentDomTopic.topicName == prevDomTopic.topicName) {
+							isOldTopic = true;
+							const subChanges = findSubChanges(prevDomTopic, currentDomTopic)
+							if (subChanges) {
+								changes.push({
+									topicName: currentDomTopic.topicName,
+									subChanges: subChanges
+								})
+							}
+
+							break;
+						}
+					});
+
+					if (!isOldTopic) {
+						changes.push({
+							topicName: currentDomTopic.topicName
+						})
+					}
+				});
+
+			}
+
+			//before exit or refresh
+			// const result = updateLocalStorage(currentDom);
 			// ----------------------------------------------------------
 
 		}
 	}, 10);
 });
 
-const updateLocalStorage = (domObj) => {
-
+const getPrevDom = (courseTitle) => {
 	const storage = localStorage.getItem("spectrum_checker");
-	if (storage) {
-		 return JSON.parse(storage);
-	} else {
-		const domData = JSON.stringify(domObj);
-		localStorage.setItem("spectrum_checker", domData)
-		return "successfully store in local storage."
+	if (storage && storage[courseTitle]) {
+		return storage[courseTitle];
 	}
+	return null;
+}
+
+const getCurrentDom = () => {
+	let topics = [];
+	$("ul.topics").children().each((i, el) => {
+		const text = el.innerText
+		topics.push({
+			topicName: text.substr(0, text.indexOf(" ")),
+			content: text.substr(text.indexOf(" "))
+		});
+	});
+
+	return {
+		topics: topics,
+		lastCheckedTime: new Date()
+	}
+
+}
+
+const findSubChanges = (prevDomTopic, currentDomTopic) => {
+	return null;
+}
+
+const storeDom = (courseTitle, currentDom) => {
+	//create new obj if not found in local storage
+	const storage = localStorage.getItem("spectrum_checker") || {};
+	storage[courseTitle] = currentDom;
+	localStorage.setItem("spectrum_checker", JSON.stringify(storage))
 }
 //TODO
 // if the spectrum is launched at not a spectrum site
-// 
+//
