0057a4e9a10d28cd305752525268b85c760fe93e
1998chun <34387770+1998chun@users.noreply.github.com>
2020-03-11 15:54:07 +0800

    WIP: traversing DOM

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,32 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from scripts/inject.js");
+			const domObj = $('#region-main');
+			console.log(domObj.children())
+			// const result = updateLocalStorage(domObj);
+			// console.log(result)
+			// ----------------------------------------------------------
 
-	}
+		}
 	}, 10);
 });
+
+const updateLocalStorage = (domObj) => {
+
+	const storage = localStorage.getItem("spectrum_checker");
+	if (storage) {
+		 return JSON.parse(storage);
+	} else {
+		const domData = JSON.stringify(domObj);
+		localStorage.setItem("spectrum_checker", domData)
+		return "successfully store in local storage."
+	}
+}
+//TODO
+// if the spectrum is launched at not a spectrum site
+// 
